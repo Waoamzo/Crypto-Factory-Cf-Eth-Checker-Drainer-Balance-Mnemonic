a82a10fab556985b3283059b0953ccca706d742b
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:31:57 +0100

    End.cs

---
+++ FructoseCheckerV1/FructoseLibrary/CLI/Visual/Static/End.cs
@@ -0,0 +1,22 @@
+﻿using Colorful;
+
+using FructoseLib.Utils;
+
+using Console = Colorful.Console;
+
+namespace FructoseLib.CLI.Visual.Static
+{
+    static partial class ConsoleComponents
+    {
+        public static void RenderEnd(long Total)
+        {
+            Formatter[] StartFormat = new Formatter[]
+            {
+                new Formatter($"{Total}", System.Drawing.Color.BlueViolet),
+                new Formatter($"{Diagnostics.ElapsedSeconds() / 60} min {Diagnostics.ElapsedSeconds() % 60} sec", System.Drawing.Color.Gray),
+            };
+
+            Console.WriteLineFormatted(" \n Succesfuly drained {0} wallets in {1}", System.Drawing.Color.White, StartFormat);
+        }
+    }
+}

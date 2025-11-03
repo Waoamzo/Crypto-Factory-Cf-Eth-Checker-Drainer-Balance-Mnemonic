8620fb8cbd0e79bfaf2b7f08d239b5e21762d75d
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:58 +0100

    UsedChains.cs

---
+++ FructoseCheckerV1/Debank/Types/UsedChains.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FructoseCheckerV1.Debank.Types
+{
+    internal class Chains
+    {
+        [JsonProperty("chains")]
+        public List<string> UsedChains { get; private set; }
+
+        public Chains()
+        {
+            this.UsedChains = new();
+        }
+    }
+}

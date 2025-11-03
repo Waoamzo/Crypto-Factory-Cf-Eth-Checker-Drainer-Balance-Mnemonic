989813e97bfd61615bedc8046218d8e648bf799a
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:12 +0100

    TransactionOutputValue.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionBody/TransactionOutputValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.Models.Transactions
+{
+    public partial class TransactionOutputValue
+    {
+        public ulong Coin { get; set; }
+        /// <summary>
+        /// BPlusTree<byte[], NativeAsset>
+        /// byte[] = PolicyID
+        /// </summary>
+        public Dictionary<byte[], NativeAsset> MultiAsset { get; set; }
+    }
+}

d437b85da310f624c89c00a7fada8c602fff9d53
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:23 +0100

    TransactionOutputValueBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/TransactionOutputValueBuilder.cs
@@ -0,0 +1,46 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface ITransactionOutputValueBuilder: IABuilder<TransactionOutputValue>
+    {
+        ITransactionOutputValueBuilder WithCoin(uint coin);
+
+        ITransactionOutputValueBuilder WithMultiAsset(Dictionary<byte[], NativeAsset> multiAsset);
+    }
+
+    public class TransactionOutputValueBuilder: ABuilder<TransactionOutputValue>, ITransactionOutputValueBuilder
+    {
+        public TransactionOutputValueBuilder()
+        {
+            _model = new TransactionOutputValue();
+        }
+
+        private TransactionOutputValueBuilder(TransactionOutputValue model)
+        {
+            _model = model;
+        }
+
+        public static ITransactionOutputValueBuilder GetBuilder(TransactionOutputValue model)
+        {
+            if (model == null)
+            {
+                return new TransactionOutputValueBuilder();
+            }
+            return new TransactionOutputValueBuilder(model);
+        }
+
+        public ITransactionOutputValueBuilder WithCoin(uint coin)
+        {
+            _model.Coin = coin;
+            return this;
+        }
+
+        public ITransactionOutputValueBuilder WithMultiAsset(Dictionary<byte[], NativeAsset> multiAsset)
+        {
+            _model.MultiAsset = multiAsset;
+            return this;
+        }
+    }
+}

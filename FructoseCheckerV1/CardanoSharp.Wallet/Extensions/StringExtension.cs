d4651661e9c46fabc1cd6777d3afb0f437a4ba4d
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:33 +0100

    StringExtension.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Extensions/StringExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CardanoSharp.Wallet.Extensions
+{
+	public static class StringExtension
+	{
+		public static string ToStringHex(this byte[] bytes)
+		{
+			var hex = BitConverter
+				.ToString(bytes)
+				.Replace("-", "")
+				.ToLower();
+
+			return hex;
+		}
+
+		public static string GetString(this byte[] bytes)
+		{
+			return System.Text.Encoding.UTF8.GetString(bytes);
+		}
+
+		public static byte[] ToBytes(this string value)
+		{
+			return System.Text.Encoding.UTF8.GetBytes(value);
+		}
+	}
+}

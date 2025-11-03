1f1f588c6c3080dbcb3b17bfce6225f3d7deafc3
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:48 +0100

    ScriptNofK.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionWitness/Scripts/ScriptNofK.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.Models.Transactions.Scripts
+{
+    // script_n_of_k = (3, n: uint, [ * native_script ])
+    public class ScriptNofK
+    {
+        public ScriptNofK()
+        {
+            NativeScripts = new List<NativeScript>();
+        }
+
+        public uint N { get; set; }
+        public IList<NativeScript> NativeScripts { get; set; }
+    }
+}

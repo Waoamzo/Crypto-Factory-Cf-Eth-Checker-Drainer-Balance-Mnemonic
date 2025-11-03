2ade97a97e718cc3923dc0a3d68f9d592f2d656b
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:03 +0100

    NativeAsset.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionBody/NativeAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.Models.Transactions
+{
+    public partial class NativeAsset
+    {
+        public NativeAsset()
+        {
+            Token = new Dictionary<byte[], long>();
+        }
+
+        public Dictionary<byte[], long> Token { get; set; }
+    }
+}

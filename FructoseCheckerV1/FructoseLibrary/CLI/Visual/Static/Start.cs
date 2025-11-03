2fd988c499186c840fec7592f306a9b3a69077f5
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:01 +0100

    Start.cs

---
+++ FructoseCheckerV1/FructoseLibrary/CLI/Visual/Static/Start.cs
@@ -0,0 +1,21 @@
+﻿using Colorful;
+using FructoseLib.Utils;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace FructoseLib.CLI.Visual.Static
+{
+    static partial class ConsoleComponents
+    {
+        public static void RenderStart(int Threads)
+        {
+            Formatter[] StartFormat = new Formatter[]
+            {
+                new Formatter($"{Threads}", System.Drawing.Color.BlueViolet),
+            };
+
+            Console.WriteLineFormatted(" Start checking on {0} threads...\n", System.Drawing.Color.White, StartFormat);
+            Diagnostics.StartTimer();
+        }
+    }
+}

420b8105e19ffb37ee2681f8bb621e16ece5c4b7
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:42 +0100

    ScriptAny.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionWitness/Scripts/ScriptAny.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.Models.Transactions.Scripts
+{
+    // script_any = (2, [ * native_script ])
+    public class ScriptAny
+    {
+        public ScriptAny()
+        {
+            NativeScripts = new List<NativeScript>();
+        }
+
+        public IList<NativeScript> NativeScripts { get; set; }
+    }
+}

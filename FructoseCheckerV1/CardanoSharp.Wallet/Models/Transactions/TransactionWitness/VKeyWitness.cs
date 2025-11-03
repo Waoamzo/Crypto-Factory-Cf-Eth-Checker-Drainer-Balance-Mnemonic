2fe7bddd89bb629aa5021ea4cde09b0694e782ff
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:39 +0100

    VKeyWitness.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionWitness/VKeyWitness.cs
@@ -0,0 +1,13 @@
+﻿using CardanoSharp.Wallet.Models.Keys;
+
+namespace CardanoSharp.Wallet.Models.Transactions
+{
+    //vkeywitness = [ $vkey, $signature]
+    public partial class VKeyWitness
+    {
+        public PublicKey VKey { get; set; }
+        public PrivateKey SKey { get; set; }
+        public byte[] Signature { get; set; }
+        public bool IsMock { get; set; }
+    }
+}

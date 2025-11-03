46343e29ad3dc65d256eb7cd7ca061e7ddc86cc7
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:45 +0100

    EthereumPrivateKey.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Parser/Ethereum/Types/PrivateKey/EthereumPrivateKey.cs
@@ -0,0 +1,25 @@
+﻿using FructoseLib.Types.Ethereum.Enums;
+using System.Collections.Generic;
+
+namespace FructoseLib.Types.Ethereum.Types
+{
+    public struct EthereumPrivateKey : IEthereumUnit
+    {
+        public EthereumPrivateKey(string PrivateKey, (string Hash, long Line) Metadata)
+        {
+            this.Type = EthereumUnitType.PrivateKey;
+            this.Value = "0x" + PrivateKey;
+            this.Accounts = Generate(this.Value);
+            this.Metadata = Metadata;
+        }
+
+        public EthereumUnitType Type { get; init; }
+        public string Value { get; init; }
+        public IEnumerable<EthereumAccount> Accounts { get; init; }
+        public (string Hash, long Line) Metadata { get; init; }
+        private static IEnumerable<EthereumAccount> Generate(string Key)
+        {
+            yield return new(Nethereum.Signer.EthECKey.GetPublicAddress(Key), Key, $"Unknown") ;
+        }
+    }
+}

8a7a5182c8712d52d02acc8b22150d0c607809c2
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:26 +0100

    VKeyWitnessBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/VKeyWitnessBuilder.cs
@@ -0,0 +1,54 @@
+﻿using CardanoSharp.Wallet.Models.Keys;
+using CardanoSharp.Wallet.Models.Transactions;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IVKeyWitnessBuilder: IABuilder<VKeyWitness>
+    {
+        IVKeyWitnessBuilder WithVKey(PublicKey vKey);
+
+        IVKeyWitnessBuilder WithSKey(PrivateKey sKey);
+
+        IVKeyWitnessBuilder WithSignature(byte[] signature);
+    }
+
+    public class VKeyWitnessBuilder: ABuilder<VKeyWitness>, IVKeyWitnessBuilder
+    {
+        public VKeyWitnessBuilder()
+        {
+            _model = new VKeyWitness();
+        }
+
+        private VKeyWitnessBuilder(VKeyWitness model)
+        {
+            _model = model;
+        }
+
+        public static IVKeyWitnessBuilder GetBuilder(VKeyWitness model = null)
+        {
+            if (model == null)
+            {
+                return new VKeyWitnessBuilder();
+            }
+            return new VKeyWitnessBuilder(model);
+        }
+
+        public IVKeyWitnessBuilder WithVKey(PublicKey vKey)
+        {
+            _model.VKey = vKey;
+            return this;
+        }
+
+        public IVKeyWitnessBuilder WithSKey(PrivateKey sKey)
+        {
+            _model.SKey = sKey;
+            return this;
+        }
+
+        public IVKeyWitnessBuilder WithSignature(byte[] signature)
+        {
+            _model.Signature = signature;
+            return this;
+        }
+    }
+}

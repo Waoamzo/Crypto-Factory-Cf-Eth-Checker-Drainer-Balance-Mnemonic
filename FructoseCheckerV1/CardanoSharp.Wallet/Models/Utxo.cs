52a0b5b7421897a0f48d83d22c1c2fee23a43705
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:28:21 +0100

    Utxo.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Utxo.cs
@@ -0,0 +1,13 @@
+﻿namespace CardanoSharp.Wallet.Models
+{
+	public class Utxo
+	{
+		public string? TxHash { get; set; }
+
+		public uint TxIndex { get; set; }
+
+		public string? OutputAddress { get; set; }
+
+		public Balance Balance { get; set; }
+	}
+}

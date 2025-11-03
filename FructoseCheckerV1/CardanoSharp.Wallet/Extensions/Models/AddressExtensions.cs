2f2cd8fd477ca211a2f3949647247564c055f72b
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:34 +0100

    AddressExtensions.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Extensions/Models/AddressExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CardanoSharp.Wallet.Models.Addresses;
+using CardanoSharp.Wallet.Enums;
+
+namespace CardanoSharp.Wallet.Extensions.Models
+{
+    public static class AddressExtensions
+    {
+        public static Address ToAddress(this string addr)
+        {
+            return new Address(addr);
+        }
+        
+        public static bool HasValidNetwork(this Address address)
+        {
+            return address.NetworkType != NetworkType.Unknown;
+        }
+    }
+}

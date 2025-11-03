68c7e2b2905cf98d0d5db68e66a41a12f19018e5
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:19 +0100

    NetworkId.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Enums/NetworkId.cs
@@ -0,0 +1,11 @@
+﻿namespace CardanoSharp.Wallet.Enums
+{
+    /// <summary>
+    /// https://github.com/cardano-foundation/CIPs/blob/master/CIP-0019/CIP-0019.md#network-tag
+    /// </summary>
+    public enum NetworkId
+    {
+        Testnet = 0b0000,
+        Mainnet = 0b0001
+    }
+}

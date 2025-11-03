570830e313b8e6ef953be5e069ac0a5539755849
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:41 +0100

    IEthereumUnit.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Parser/Ethereum/Types/Interface/IEthereumUnit.cs
@@ -0,0 +1,14 @@
+﻿using FructoseLib.Types.Ethereum.Enums;
+using FructoseLib.Types.Ethereum.Types;
+using System.Collections.Generic;
+
+namespace FructoseLib.Types.Ethereum
+{
+    public interface IEthereumUnit
+    {
+        public EthereumUnitType Type { get; }
+        public string Value { get; }
+        public IEnumerable<EthereumAccount> Accounts { get; }
+        public (string Hash, long Line) Metadata { get; }
+    }
+}

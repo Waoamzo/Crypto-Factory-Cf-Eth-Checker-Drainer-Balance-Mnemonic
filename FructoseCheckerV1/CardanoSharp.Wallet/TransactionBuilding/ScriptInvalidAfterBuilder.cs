48dc741c1f4b486544180ae5ef06dede109e422d
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:05 +0100

    ScriptInvalidAfterBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/ScriptInvalidAfterBuilder.cs
@@ -0,0 +1,37 @@
+﻿using CardanoSharp.Wallet.Models.Transactions.TransactionWitness.Scripts;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IScriptInvalidAfterBuilder: IABuilder<ScriptInvalidAfter>
+    {
+        IScriptInvalidAfterBuilder WithAfter(uint after);
+    }
+
+    public class ScriptInvalidAfterBuilder: ABuilder<ScriptInvalidAfter>, IScriptInvalidAfterBuilder
+    {
+        public ScriptInvalidAfterBuilder()
+        {
+            _model = new ScriptInvalidAfter();
+        }
+
+        private ScriptInvalidAfterBuilder(ScriptInvalidAfter model)
+        {
+            _model = model;
+        }
+
+        public static IScriptInvalidAfterBuilder GetBuilder(ScriptInvalidAfter model)
+        {
+            if (model == null)
+            {
+                return new ScriptInvalidAfterBuilder();
+            }
+            return new ScriptInvalidAfterBuilder(model);
+        }
+
+        public IScriptInvalidAfterBuilder WithAfter(uint after)
+        {
+            _model.After = after;
+            return this;
+        }
+    }
+}

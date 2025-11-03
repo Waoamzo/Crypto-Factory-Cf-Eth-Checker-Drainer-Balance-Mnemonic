92b1ccfc36c9e3f16a6fd1f6cdf8bb3010064788
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:54 +0100

    Protocol.cs

---
+++ FructoseCheckerV1/Debank/Types/Protocol.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FructoseCheckerV1.Debank.Types
+{
+    public class Holdings
+    {
+        [JsonProperty("stats")]
+        private Balances Stats { get; set; }
+        public double Balance { get => this.Stats.TotalUsdValue ?? 0.0d; }
+    }
+
+    public class Protocol
+    {
+        [JsonProperty("chain")]
+        public string Chain { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("id")]
+        public string Id { get; private set; }
+
+
+        [JsonProperty("portfolio_item_list")]
+        private List<Holdings> Holdings { get; set; }
+
+        public double Balance { get => Math.Round(this.Holdings.Sum(Holding => Holding.Balance), 2); }
+    }
+
+    public class Balances
+    {
+        [JsonProperty("asset_usd_value")]
+        public double? TotalUsdValue { get; private set; }
+    }
+}

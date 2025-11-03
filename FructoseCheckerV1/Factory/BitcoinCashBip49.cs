f1d33de983f7f1cd62b7f408a16c9329b6e33f85
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:31:11 +0100

    BitcoinCashBip49.cs

---
+++ FructoseCheckerV1/Factory/BitcoinCashBip49.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+using System.Threading.Tasks;
+
+using FructoseCheckerV1.Models;
+
+using Nethereum.Util;
+
+using Newtonsoft.Json.Linq;
+
+using Amount = FructoseCheckerV1.Models.Pair<double, double>;
+
+namespace FructoseCheckerV1.Factory
+{
+    public class BitcoinCashBip49 : WalletCheckerModelHttp
+    {
+        private static DateTime PriceUpdatedAt = DateTime.MinValue;
+        private static double LastPrice = 235.0d;
+
+        public BitcoinCashBip49(ref Python Python, bool SelfCheck = false)
+            : base(ref Python, CoinType.BCH_BIP49, SelfCheck)
+        {
+            Url = "https://bitcoin-cash-node.atomicwallet.io/api/v2/xpub/{0}?&details=tokenBalances&tokens=used";
+            SelfCheckAddress = "xpub6CzNoCaNinBXk4eXhjefkWbjyYFH57QhfJ284XhbnMHdR9djM11WD7HKFapr1NUxaexYFKoRkSn4HsGrZPQK9L1QumdJ6Wep8aHDjWA1gZa";
+            PriceUrl = "https://www.binance.com/api/v3/ticker/price?symbol=BCHUSDT";
+        }
+
+        protected override async Task<Amount> DeserializeCoinResponce(Wallet Wallet)
+        {
+            try
+            {
+                var Object = await GetResponse(GetUrl(Wallet));
+                if (Object.GetValue("balance") != null)
+                {
+                    double Balance = (double)new BigDecimal(BigInteger.Parse(Object.GetValue("balance").ToString()) / new BigDecimal(100000000));
+                    return new Amount(Balance * await DeserializePriceResponce(), Balance);
+                }
+                else
+                {
+                    return new Amount(0.0d, 0.0d);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override async Task<double> DeserializePriceResponce()
+        {
+            if (PriceUpdatedAt.AddMinutes(5) < DateTime.Now)
+            {
+                try
+                {
+                    JObject Object = await GetResponse(PriceUrl);
+                    PriceUpdatedAt = DateTime.Now;
+                    return LastPrice = Convert.ToDouble(Object.GetValue("price"), new CultureInfo("ru-RU"));
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                return LastPrice;
+            }
+
+        }
+    }
+}

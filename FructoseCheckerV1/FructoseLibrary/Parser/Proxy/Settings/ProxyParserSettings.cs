c2e7d6076bc6ef5b4d228f78f0d1774cd723f714
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:52 +0100

    ProxyParserSettings.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Parser/Proxy/Settings/ProxyParserSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FructoseLib.Parser.Proxy.Settings
+{
+    public sealed record ProxyParserSettings
+    {
+        public int MaxUrlProxyLoadingTries { get; set; } = 3;
+        public int ProxyCheckingThreads { get; set; } = 200;
+        public bool OnlyHttpMode { get; set; } = false;
+        public bool OnlyRotatingMode { get; set; } = false;
+        public bool OnlyIpv6Mode { get; set; } = true; 
+        public Func<string, Task> SetDefaultProxyLocationCallback { get; set; } = null;
+
+        
+    }
+}

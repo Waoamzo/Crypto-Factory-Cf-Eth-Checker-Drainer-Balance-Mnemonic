a8e87698fbe8abdf13ce64f0a1b1bd60048ef30f
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:30 +0100

    HashUtility.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Utilities/HashUtility.cs
@@ -0,0 +1,17 @@
+﻿using Blake2Fast;
+
+namespace CardanoSharp.Wallet.Utilities
+{
+    public static class HashUtility
+    {
+        public static byte[] Blake2b224(byte[] data)
+        {
+            return Blake2b.ComputeHash(28, data);
+        }
+
+        public static byte[] Blake2b256(byte[] data)
+        {
+            return Blake2b.ComputeHash(32, data);
+        }
+    }
+}

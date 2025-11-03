c9a69e1074eca64572395cf6f1aa2e838819776f
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:17 +0100

    TransactionBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/TransactionBuilder.cs
@@ -0,0 +1,56 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface ITransactionBuilder: IABuilder<Transaction>
+    {
+        ITransactionBuilder SetBody(ITransactionBodyBuilder bodyBuilder);
+        ITransactionBuilder SetWitnesses(ITransactionWitnessSetBuilder witnessBuilder);
+        ITransactionBuilder SetAuxData(IAuxiliaryDataBuilder auxDataBuilder);
+    }
+
+    public partial class TransactionBuilder : ABuilder<Transaction>, ITransactionBuilder
+    {
+        private TransactionBuilder()
+        {
+            _model = new Transaction();
+        }
+
+        private TransactionBuilder(Transaction model)
+        {
+            _model = model;
+        }
+
+        public static ITransactionBuilder GetBuilder(Transaction model)
+        {
+            if (model == null)
+            {
+                return new TransactionBuilder();
+            }
+            return new TransactionBuilder(model);
+        }
+
+        public static ITransactionBuilder Create
+        {
+            get => new TransactionBuilder();
+        }
+
+        public ITransactionBuilder SetAuxData(IAuxiliaryDataBuilder auxDataBuilder)
+        {
+            _model.AuxiliaryData = auxDataBuilder.Build();
+            return this;
+        }
+
+        public ITransactionBuilder SetBody(ITransactionBodyBuilder bodyBuilder)
+        {
+            _model.TransactionBody = bodyBuilder.Build();
+            return this;
+        }
+
+        public ITransactionBuilder SetWitnesses(ITransactionWitnessSetBuilder witnessBuilder)
+        {
+            _model.TransactionWitnessSet = witnessBuilder.Build();
+            return this;
+        }
+    }
+}

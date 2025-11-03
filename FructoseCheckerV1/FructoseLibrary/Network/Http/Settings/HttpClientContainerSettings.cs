29b291095fd205f96d78de8e41aca7dea1a5b177
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:16 +0100

    HttpClientContainerSettings.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Network/Http/Settings/HttpClientContainerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using FructoseLib.Network.Http;
+
+namespace FructoseLib.Network.Http.Settings
+{
+    public sealed class HttpClientContainerSettings
+    {
+        public bool UseProxy = true;
+        public TimeSpan Timeout = TimeSpan.FromMilliseconds(30000);
+        public long Throttling = 0;
+    }
+}

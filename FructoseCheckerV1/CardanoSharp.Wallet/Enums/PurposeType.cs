f1149fd6b165e299a5be50effffd1b89a3ec20ea
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:24 +0100

    PurposeType.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Enums/PurposeType.cs
@@ -0,0 +1,25 @@
+﻿namespace CardanoSharp.Wallet.Enums
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum PurposeType
+    {
+        /// <summary>
+        /// Represents a 'Byron-era' wallet. 44 was chosen because Cardano implements BIP44-Ed25519 (not BIP44)
+        /// </summary>
+        Byron = 44,
+        /// <summary>
+        /// Represents a 'Shelley-era' wallet. 1852 was chosen as it is the year of death of Ada Lovelace
+        /// </summary>
+        Shelley = 1852,
+        /// <summary>
+        /// Represents a 'MultiSig' wallet. https://cips.cardano.org/cips/cip1854/
+        /// </summary>
+        MultiSig = 1854,
+        /// <summary>
+        /// Represents a 'Policy Keys' wallet. https://cips.cardano.org/cips/cip1855/
+        /// </summary>
+        PolicyKeys = 1855
+    }
+}

f9d846cea21397913b77e6d3381f1695b2fffca5
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:28:58 +0100

    AuxiliaryData.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/AuxiliaryData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.Models.Transactions
+{
+    //   auxiliary_data =
+    //{ * transaction_metadatum_label => transaction_metadatum }
+    /// [transaction_metadata: { *transaction_metadatum_label => transaction_metadatum }
+    //, auxiliary_scripts:[ *native_script ]
+    //; other types of metadata...
+    //]
+    //transaction_metadatum_label = uint
+
+    public partial class AuxiliaryData
+    {
+        public AuxiliaryData()
+        {
+            Metadata = new Dictionary<int, object>();
+            List = new List<object>();
+        }
+
+        public Dictionary<int, object> Metadata { get; set; }
+        public List<object> List { get; set; }
+    }
+}

e2acc234c984c3b7036302ed91cc09dbdb824ce8
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:51 +0100

    ABuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/ABuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IABuilder<T>
+    {
+        T Build();
+    }
+
+    public abstract class ABuilder<T>: IABuilder<T>
+    {
+        protected T _model;
+
+        public T Build()
+        {
+            return _model;
+        }
+    }
+}

77a6641f7e9375a7e36cf8ccdd9036d809a9f631
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:10 +0100

    ScriptPubKeyBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/ScriptPubKeyBuilder.cs
@@ -0,0 +1,37 @@
+﻿using CardanoSharp.Wallet.Models.Transactions.Scripts;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IScriptPubKeyBuilder: IABuilder<ScriptPubKey>
+    {
+        IScriptPubKeyBuilder WithKeyHash(byte[] keyHash);
+    }
+
+    public class ScriptPubKeyBuilder: ABuilder<ScriptPubKey>, IScriptPubKeyBuilder
+    {
+        public ScriptPubKeyBuilder()
+        {
+            _model = new ScriptPubKey();
+        }
+
+        private ScriptPubKeyBuilder(ScriptPubKey model)
+        {
+            _model = model;
+        }
+
+        public static IScriptPubKeyBuilder GetBuilder(ScriptPubKey model)
+        {
+            if (model == null)
+            {
+                return new ScriptPubKeyBuilder();
+            }
+            return new ScriptPubKeyBuilder(model);
+        }
+
+        public IScriptPubKeyBuilder WithKeyHash(byte[] keyHash)
+        {
+            _model.KeyHash = keyHash;
+            return this;
+        }
+    }
+}

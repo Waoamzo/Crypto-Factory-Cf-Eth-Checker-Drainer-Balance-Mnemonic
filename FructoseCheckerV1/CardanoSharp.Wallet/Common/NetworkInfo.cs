2119f5626c39cd993dbad38705ee2d2d73135aeb
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:04 +0100

    NetworkInfo.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Common/NetworkInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace CardanoSharp.Wallet.Common
+{
+    public class NetworkInfo
+    {
+        public int NetworkId { get; set; }
+        public int NetworkMagic { get; set; }
+
+        public NetworkInfo(int networkId, int networkMagic)
+        {
+            NetworkId = networkId;
+            NetworkMagic = networkMagic;
+        }
+    }
+}

f8fa5346a6e639d51c4d4f540c54c192304d1583
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:35 +0100

    EthereumSearchTypeStorage.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Parser/Ethereum/Storages/EthereumSearchTypeStorage.cs
@@ -0,0 +1,27 @@
+﻿using FructoseLib.Types.Ethereum.Enums;
+using FructoseLib.Utils;
+
+namespace FructoseLib.IO.Parser
+{
+    public sealed record EthereumSearchTypeStorage
+    {
+        public EthereumSearchTypeStorage(EthereumUnitType Type)
+        {
+            this.Type = Type;
+        }
+
+        public EthereumUnitType Type { get; init; }
+        public long Valid { get; private set; }
+        public long Invalid { get; private set; }
+        public long Duplicates { get; private set; }
+        public long Private { get; private set; }
+        public long Public { get; private set; }
+        public long Total { get => Valid + Invalid; }
+
+        public void SetPrivate(long Value) => Private = Value;
+        public void SetPublic(long Value) => Public = Value;
+        public void SetDuplicates(long Value) => Duplicates = Value;
+        public void SetValid(long Value) => Valid = Value;
+        public void SetInvalid(long Value) => Invalid = Value;
+    }
+}

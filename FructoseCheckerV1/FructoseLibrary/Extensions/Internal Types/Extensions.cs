2afd28b26f43af38a896f50de6612f0954615b19
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:05 +0100

    Extensions.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Extensions/Internal Types/Extensions.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace FructoseLib.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> Source)
+        {
+            return Source.Shuffle(new System.Random());
+        }
+        private static IEnumerable<T> Shuffle<T>(this IEnumerable<T> Source, System.Random Random)
+        {
+            if (Source == null) throw new ArgumentNullException(nameof(Source));
+            if (Random == null) throw new ArgumentNullException(nameof(Random));
+
+            return Source.ShuffleIterator(Random);
+        }
+        private static IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> Source, System.Random Random)
+        {
+            var Buffer = Source.ToArray();
+            for (int i = 0; i < Buffer.Length; i++)
+            {
+                int j = Random.Next(i, Buffer.Length);
+                yield return Buffer[j];
+
+                Buffer[j] = Buffer[i];
+            }
+        }
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> Source, int Size)
+        {
+            using var Enumerator = Source.GetEnumerator();
+            while (Enumerator.MoveNext())
+            {
+                yield return YieldBatch(Enumerator, Size - 1);
+            }
+        }
+        private static IEnumerable<T> YieldBatch<T>(IEnumerator<T> Source, int Size)
+        {
+            yield return Source.Current;
+            for (int i = 0; i < Size && Source.MoveNext(); i++)
+            {
+                yield return Source.Current;
+            }
+        }
+        public static int IndexOf<T>(this IEnumerable<T> Source, T Value)
+        {
+            int Index = 0;
+            var Comparer = EqualityComparer<T>.Default;
+            foreach (T Item in Source)
+            {
+                if (Comparer.Equals(Item, Value)) return Index;
+                Index++;
+            }
+            return 0;
+        }
+        public static T Random<T>(this IEnumerable<T> Collection)
+        {
+            return Collection.ElementAtOrDefault(new System.Random().Next(0, Collection.Count()));
+        }
+        public static IEnumerable<(T Item, int Index)> WithIndex<T>(this IEnumerable<T> Source)
+        {
+            return Source.Select((Item, Index) => (Item, Index));
+        }
+    }
+
+    public static class IDictionaryExtensions
+    {
+        public static IDictionary<TKey, TValue> AddRange<TKey, TValue>(this IDictionary<TKey, TValue> Left, IDictionary<TKey, TValue> Right)
+        {
+            foreach (var Pair in Right)
+            {
+                Left.Add(Pair);
+            }
+
+            return Left;
+        }
+    }
+
+    public static class RandomExtensions
+    {
+        public enum Alphabet { Standart, HexLower, HexUpper, HexCombine, Special };
+        private static readonly string[] UserAgents = new string[]
+        {
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.47",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 OPR/92.0.0.0",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.77",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0",
+            "Mozilla/5.0 (Windows NT 10.0; rv:105.0) Gecko/20100101 Firefox/105.0",
+            "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
+            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 OPR/91.0.4516.106",
+            "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.24",
+            "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Mozilla/5.0 (Windows NT 10.0; rv:107.0) Gecko/20100101 Firefox/107.0",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.124 YaBrowser/22.9.5.710 Yowser/2.5 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+        };
+        public static string GetAlphabet(this Alphabet Value) => Value switch
+        {
+            Alphabet.Standart => "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+            Alphabet.Special => "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz%*),?@#$~",
+            Alphabet.HexLower => "0123456789abcdef",
+            Alphabet.HexUpper => "0123456789ABCDEF",
+            Alphabet.HexCombine => "0123456789abcdefABCDEF",
+            _ => throw new NotSupportedException()
+        };
+
+        public static string String(this Random Random, int Lenght, Alphabet AlphabetType = Alphabet.HexLower)
+        {
+            string Alphabet = AlphabetType.GetAlphabet();
+            char[] RandomString = new char[Lenght];
+
+            for (int Index = 0; Index < RandomString.Length; Index++)
+            {
+                RandomString[Index] = Alphabet[Random.Next(Alphabet.Length - 1)];
+            }
+
+            return new string(RandomString);
+        }
+        public static string Name(this Random Random, int Lenght)
+        {
+            string[] Consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] Vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string Name = "";
+            Name += Consonants[Random.Next(Consonants.Length)].ToUpper();
+            Name += Vowels[Random.Next(Vowels.Length)];
+            int LetterAddedCount = 2;
+            while (LetterAddedCount < Lenght)
+            {
+                Name += Consonants[Random.Next(Consonants.Length)];
+                LetterAddedCount++;
+                Name += Vowels[Random.Next(Vowels.Length)];
+                LetterAddedCount++;
+            }
+
+            return Name;
+
+
+        }
+        public static string UserAgent(this Random Random)
+        {
+            return UserAgents.Random();
+        }
+
+    }
+
+    public static class WebProxyExtensions
+    {
+        public static string? GetUsername(this System.Net.WebProxy WebProxy)
+        {
+            return ((NetworkCredential)WebProxy.Credentials)?.UserName;
+        }
+
+        public static string? GetPassword(this System.Net.WebProxy WebProxy)
+        {
+            return ((NetworkCredential)WebProxy.Credentials)?.Password;
+        }
+
+        public static string ToOriginalString(this System.Net.WebProxy WebProxy)
+        {
+            return $"{WebProxy.Address.ToString().Remove(WebProxy.Address.ToString().Length - 1, 1)}:{WebProxy.GetUsername()}:{WebProxy.GetPassword()}"; 
+        }
+    }
+}

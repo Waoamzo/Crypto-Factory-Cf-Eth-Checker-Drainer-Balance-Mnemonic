29b6388d125ed446050484755efacff5844481a3
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:12 +0100

    StakeDelegationBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/StakeDelegationBuilder.cs
@@ -0,0 +1,45 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IStakeDelegationBuilder: IABuilder<StakeDelegation>
+    {
+        IStakeDelegationBuilder WithStakeCredential(byte[] stakeCredential);
+
+        IStakeDelegationBuilder WithPoolHash(byte[] poolHash);
+    }
+
+    public class StakeDelegationBuilder: ABuilder<StakeDelegation>, IStakeDelegationBuilder
+    {
+        public StakeDelegationBuilder()
+        {
+            _model = new StakeDelegation();
+        }
+
+        private StakeDelegationBuilder(StakeDelegation model)
+        {
+            _model = model;
+        }
+
+        public static IStakeDelegationBuilder GetBuilder(StakeDelegation model)
+        {
+            if (model == null)
+            {
+                return new StakeDelegationBuilder();
+            }
+            return new StakeDelegationBuilder(model);
+        }
+
+        public IStakeDelegationBuilder WithStakeCredential(byte[] stakeCredential)
+        {
+            _model.StakeCredential = stakeCredential;
+            return this;
+        }
+
+        public IStakeDelegationBuilder WithPoolHash(byte[] poolHash)
+        {
+            _model.PoolHash = poolHash;
+            return this;
+        }
+    }
+}

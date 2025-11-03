16ae6c0910225c6569786c48297d11d38a1ff806
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:19 +0100

    HttpRequestFactorySettings.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Network/Http/Settings/HttpRequestFactorySettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FructoseLib.Network.Http.Settings
+{
+    public class HttpRequestFactorySettings
+    {
+        public bool UseProxy = true;
+        public TimeSpan Timeout = TimeSpan.FromMilliseconds(30000);
+        public int MaxConcurrentPeeks = 1;
+        public long Rate = 60;
+        public TimeSpan InTime = TimeSpan.FromSeconds(60);
+    }
+}

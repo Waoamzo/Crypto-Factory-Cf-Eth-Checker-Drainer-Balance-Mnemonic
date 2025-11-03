11aa81a297f22a92d8e96b62b935c2d34ee468cb
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:27:03 +0100

    FeeStructure.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Common/FeeStructure.cs
@@ -0,0 +1,9 @@
+﻿namespace CardanoSharp.Wallet.Common
+{
+    public static class FeeStructure
+    {
+        // Constants taken from protocol params as of mainnet epoch 345
+        public const uint Coefficient = 44;
+        public const uint Constant = 155381;
+    }
+}

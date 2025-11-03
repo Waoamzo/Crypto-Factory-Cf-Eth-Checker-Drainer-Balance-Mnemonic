4586283c09c187b1fdd63f9ea4a73ccc90256c03
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:49 +0100

    ScriptPubKey.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Transactions/TransactionWitness/Scripts/ScriptPubKey.cs
@@ -0,0 +1,8 @@
+﻿namespace CardanoSharp.Wallet.Models.Transactions.Scripts
+{
+    public class ScriptPubKey
+    {
+        // addr_keyhash = $hash28
+        public byte[] KeyHash { get; set; }
+    }
+}

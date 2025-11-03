9d7f65ee3cb4b0363b3a6344afb1ae4e1c59dad7
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:28:42 +0100

    PrivateKey.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Keys/PrivateKey.cs
@@ -0,0 +1,14 @@
+﻿namespace CardanoSharp.Wallet.Models.Keys
+{
+    public class PrivateKey
+    {
+        public byte[] Key { get; }
+        public byte[] Chaincode { get; }
+
+        public PrivateKey(byte[] key, byte[] chaincode)
+        {
+            Key = key;
+            Chaincode = chaincode;
+        }
+    }
+}

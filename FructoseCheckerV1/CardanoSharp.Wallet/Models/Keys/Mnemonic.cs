5a73d1e3e33524c0fc0bd9c2fef1ff8c207085ff
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:28:40 +0100

    Mnemonic.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/Models/Keys/Mnemonic.cs
@@ -0,0 +1,14 @@
+﻿namespace CardanoSharp.Wallet.Models.Keys
+{
+    public class Mnemonic
+    {
+        public string Words { get; }
+        public byte[] Entropy { get; }
+
+        public Mnemonic(string words, byte[] entropy)
+        {
+            Words = words;
+            Entropy = entropy;
+        }
+    }
+}

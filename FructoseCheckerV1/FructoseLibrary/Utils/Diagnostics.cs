431abd38ceca07f55bb54c9d529fb00802c90117
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:32:54 +0100

    Diagnostics.cs

---
+++ FructoseCheckerV1/FructoseLibrary/Utils/Diagnostics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FructoseLib.Utils
+{
+    public static class Diagnostics
+    {
+        static Diagnostics()
+        {
+            Timer = new Stopwatch();
+        }
+
+        private static Stopwatch Timer { get; set; }
+        public static int[] GetThreadsIndex(int Count)
+        {
+            int[] Array = new int[Count];
+            for (int i = 0; i <= Count - 1; i++)
+            {
+                Array[i] = i + 1;
+            }
+
+            return Array;
+        }
+
+        public static void InfiniteWait()
+        {
+            while (true)
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static void StartTimer()
+        {
+            Timer.Start();
+        }
+        public static long ElapsedSeconds() { return Timer.ElapsedMilliseconds / 1000; }
+        public static long ElapsedMilliseconds() { return Timer.ElapsedMilliseconds; }
+
+    }
+}

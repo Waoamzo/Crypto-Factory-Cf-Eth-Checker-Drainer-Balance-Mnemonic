8272fd35f8e29894b843550b2301a79b22b3fd5a
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:07 +0100

    ScriptInvalidBeforeBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/ScriptInvalidBeforeBuilder.cs
@@ -0,0 +1,37 @@
+﻿using CardanoSharp.Wallet.Models.Transactions.TransactionWitness.Scripts;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IScriptInvalidBeforeBuilder: IABuilder<ScriptInvalidBefore>
+    {
+        IScriptInvalidBeforeBuilder WithBefore(uint before);
+    }
+
+    public class ScriptInvalidBeforeBuilder: ABuilder<ScriptInvalidBefore>, IScriptInvalidBeforeBuilder
+    {
+        public ScriptInvalidBeforeBuilder()
+        {
+            _model = new ScriptInvalidBefore();
+        }
+
+        private ScriptInvalidBeforeBuilder(ScriptInvalidBefore model)
+        {
+            _model = model;
+        }
+
+        public static IScriptInvalidBeforeBuilder GetBuilder(ScriptInvalidBefore model)
+        {
+            if (model == null)
+            {
+                return new ScriptInvalidBeforeBuilder();
+            }
+            return new ScriptInvalidBeforeBuilder(model);
+        }
+
+        public IScriptInvalidBeforeBuilder WithBefore(uint before)
+        {
+            _model.Before = before;
+            return this;
+        }
+    }
+}

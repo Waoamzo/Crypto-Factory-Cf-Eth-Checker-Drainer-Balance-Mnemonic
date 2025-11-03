08769fc1a377650a9d1400e81d332bb93d828bea
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:29:58 +0100

    NativeAssetBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/NativeAssetBuilder.cs
@@ -0,0 +1,38 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface INativeAssetBuilder: IABuilder<NativeAsset>
+    {
+        INativeAssetBuilder WithToken(Dictionary<byte[], long> token);
+    }
+
+    public class NativeAssetBuilder: ABuilder<NativeAsset>, INativeAssetBuilder
+    {
+        public NativeAssetBuilder()
+        {
+            _model = new NativeAsset();
+        }
+
+        private NativeAssetBuilder(NativeAsset model)
+        {
+            _model = model;
+        }
+
+        public static INativeAssetBuilder GetBuilder(NativeAsset model)
+        {
+            if (model == null)
+            {
+                return new NativeAssetBuilder();
+            }
+            return new NativeAssetBuilder(model);
+        }
+
+        public INativeAssetBuilder WithToken(Dictionary<byte[], long> token)
+        {
+            _model.Token = token;
+            return this;
+        }
+    }
+}

26b53737e3e0b02981b8643aa5dd475f7123f6c3
Denis T. <deadspell21391@gmail.com>
2025-11-03 09:30:03 +0100

    ScriptAnyBuilder.cs

---
+++ FructoseCheckerV1/CardanoSharp.Wallet/TransactionBuilding/ScriptAnyBuilder.cs
@@ -0,0 +1,51 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+using CardanoSharp.Wallet.Models.Transactions.Scripts;
+using System.Collections.Generic;
+
+namespace CardanoSharp.Wallet.TransactionBuilding
+{
+    public interface IScriptAnyBuilder: IABuilder<ScriptAny>
+    {
+        IScriptAnyBuilder SetScript(INativeScriptBuilder nativeScriptBuilder);
+        IScriptAnyBuilder WithNativeScripts(List<NativeScript> nativeScripts);
+    }
+
+    public class ScriptAnyBuilder: ABuilder<ScriptAny>, IScriptAnyBuilder
+    {
+        public ScriptAnyBuilder()
+        {
+            _model = new ScriptAny();
+        }
+
+        private ScriptAnyBuilder(ScriptAny model)
+        {
+            _model = model;
+        }
+
+        public static IScriptAnyBuilder GetBuilder(ScriptAny model)
+        {
+            if (model == null)
+            {
+                return new ScriptAnyBuilder();
+            }
+            return new ScriptAnyBuilder(model);
+        }
+
+        public static IScriptAnyBuilder Create
+        {
+            get => new ScriptAnyBuilder();
+        }
+
+        public IScriptAnyBuilder SetScript(INativeScriptBuilder nativeScriptBuilder)
+        {
+            _model.NativeScripts.Add(nativeScriptBuilder.Build());
+            return this;
+        }
+
+        public IScriptAnyBuilder WithNativeScripts(List<NativeScript> nativeScripts)
+        {
+            _model.NativeScripts = nativeScripts;
+            return this;
+        }
+    }
+}
